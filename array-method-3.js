015ba3364eeece67a18127d1e8e9dd5165836ad3
Arunkum20072002 <arunkumarmarikani10@gmail.com>
2023-12-04 11:06:30 +0530

    Array-method-3

---
+++ array-method-3.js
@@ -0,0 +1,8 @@
+document.write("3.Remove number 10 in the given array"+"<br>")
+document.write("-----------------------------------"+"<br>")
+
+let numbers = [3,4,5,6,7,8,9,10]
+document.write(numbers+"<br>")
+
+numbers.pop(10);
+document.write(numbers+"<br>")
